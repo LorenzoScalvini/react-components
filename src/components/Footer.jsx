43506c22fa2360112a3c682c4ab3668904861ead
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-27 15:41:20 +0100

    ADD: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Sed sit rem incidunt dignissimos quo, minus ullam repudiandae? 
+                 Sed, quia nesciunt.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
